d7b434111acb335da7f12aa738d96d504187505b
IMRAN RAEENI <74055781+Imran-555@users.noreply.github.com>
2022-01-27 19:48:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+                                          ## IF ELSE STATEMENT ###
+
+#IF  A GIVEN CONDITION INSIDE if WILL TRUE THEN AFTER STATEMENT WILL EXECUTE OTHER WISE NOT
+#USE OF if  STATEMENT      [ SYNTAX OF  if :-    if(condition): ]
+#FOR EXAMPLE
+a=45          # VALUE OF a IS 45
+b=25          # VALUE OF B IS 25
+if(a>b):      # MEANS IF "a" IS GREATER THAN "b"  THEN AFTER INSIDE THE if  STATEMENT WILL EXECUTE
+    print("a is greatest ")
+
+
+
+
+#USE OF else STATEMENT
+#IF if STATEMENT WILL NOT TRUE THEN ELSE STATEMENT WILL EXECUTE
+#FOR EXAMPLE
+c=-1
+d=20
+if(c>0):               # C IS MINUS 1 SO if STATEMENT WILL NOT EXECUTE
+    print("c is greater than zero ")
+else:                  # if STATEMENT IS NOT TRUE SO else STATEMENT WILL EXECUTE
+    print("d is greater than zero")
+
+
+
+
+# PROGRAM USING if else
+# TO IDENTIFY WHICH NUMBER IS GREATER
+num1=int(input(" Enter the first number :"))
+num2=int(input(" Enter the second number :"))
+if(num1>num2):
+    print("First number is GREATER")
+else:
+    print("Second number is GREATER")
+
+
+
+
+
+
